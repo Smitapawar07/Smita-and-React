4f06f8ace3bd14a23bffda32c543678814b97716
Smitapawar07 <pawarsmitas191@gmail.com>
2024-07-18 14:09:21 +0530

    components creating done

---
+++ Projects/1-todo-app-v-one/src/Components/TodoItem.jsx
@@ -0,0 +1,22 @@
+function TodoItem() {
+ return(
+<div class="container text-center">
+      <div class="row">
+        <div class="col-6">
+          Buy milk
+           </div>
+        <div class="col-4">
+          4/10/2023
+        </div>
+        <div class="col-2">
+        <button type="button" class="btn btn-danger">
+          Delete
+          </button>
+        </div>
+      </div>
+      </div>
+
+ );   
+}
+
+export default TodoItem;
