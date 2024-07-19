53eb641679bd49f87d9b2dfadaac0078eb549089
Smitapawar07 <pawarsmitas191@gmail.com>
2024-07-19 16:46:13 +0530

    fragment

---
+++ 18_Fragments/learn-fragment/src/App.jsx
@@ -0,0 +1,19 @@
+import "bootstrap/dist/css/bootstrap.min.css"
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div>Healthy Food</div>
+      <ul class="list-group">
+        <li class="list-group-item">dal</li>
+        <li class="list-group-item">chaval</li>
+        <li class="list-group-item">sprouts</li>
+        <li class="list-group-item">milk</li>
+        <li class="list-group-item">juice</li>
+      </ul>
+    </>
+  );
+}
+
+export default App
